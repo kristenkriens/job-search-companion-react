9e8acc2cc22b8f264d6613de32e2a242876df6f3
Kristen Kriens <kristen.kriens@gmail.com>
2019-04-13 22:57:36 -0400

    Remove unused Topbar styles and markup

--- src/components/Layout/Main/Topbar/Auth/Auth.js
+++ src/components/Layout/Main/Topbar/Auth/Auth.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Modal from '../../../../UI/Modal/Modal';
+import Profile from './Profile/Profile';
 import Login from './Login/Login';
 
 const Auth = (props) => {
@@ -8,23 +9,14 @@ const Auth = (props) => {
 
   return (
     <div className="topbar__auth">
-      {isAuthenticated ? (
-        <div class="topbar__auth-profile">
-          <div className="topbar__image">
-            <img src="./assets/images/blank-user.gif" alt="Blank User" />
-          </div>
-          <div>Hello, <span className="topbar__auth-name">Anonymous</span>!<i className="fa fa-angle-down" aria-hidden="true"></i></div>
-          <ul className="topbar__auth-dropdown">
-            <li className="topbar__auth-dropdown-item logout">Log Out</li>
-            <li className="topbar__auth-dropdown-item my-profile">My Profile</li>
-          </ul>
-        </div>
+      {!isAuthenticated ? (
+        <div>Hello, Anonymous! <button className="underline">(Logout)</button></div>
       ) : (
         <>
+          <button onClick={toggleModal}>Log In / Create Account</button>
           <Modal isModalOpen={isModalOpen} toggleModal={toggleModal} title="Login">
             <Login />
           </Modal>
-          <button className="login" onClick={toggleModal}>Log In / Create Account</button>
         </>
       )}
     </div>
--- src/components/Layout/Skipnav/Skipnav.js
+++ src/components/Layout/Skipnav/Skipnav.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './Skipnav.scss';
+
 const Skipnav = (props) => (
   <a href="#main" className="skipnav accessible">Skip to Main Content</a>
 )
