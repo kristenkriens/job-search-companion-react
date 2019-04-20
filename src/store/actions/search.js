65fce6224277823ae81e81b71421d4ee08a43f91
Kristen Kriens <kristen.kriens@gmail.com>
2019-04-19 22:26:14 -0400

    Minor adjustments to search actions

--- src/store/actions/search.js
+++ src/store/actions/search.js
@@ -79,12 +79,14 @@ export const search = (userAgent, userIp, query, location, country, radius, jobT
       co: country
     }
 
+    // http://api.indeed.com/ads/apisearch?publisher=1211867702868069&latlong=1&co=ca&format=json&userip=99.245.198.187&useragent=Mozilla/%2F4.0%28Firefox%29&v=2
+
     axios.post(url, searchData)
       .then((response) => {
-        dispatch(searchSuccess(response.data));
+        dispatch(searchSuccess(response));
       })
       .catch((error) => {
-        dispatch(searchFail(error.response.data.error));
+        dispatch(searchFail(error));
       });
   }
 };
