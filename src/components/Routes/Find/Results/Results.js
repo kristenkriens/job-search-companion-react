701807fbd1d2edf530b14a944de27b29f1e1cce3
Kristen Kriens <kristen.kriens@gmail.com>
2019-04-22 15:40:23 -0400

    Handle if results and show them

--- src/components/Routes/Find/Results/Results.js
+++ src/components/Routes/Find/Results/Results.js
@@ -5,11 +5,13 @@ class Results extends Component {
   render() {
     const { isAuthenticated, results } = this.props;
 
-    console.log(results);
-
     return (
       <>
-        <h3>Please fill out the search form first!</h3>
+        {results ? (
+          <pre>{JSON.stringify(results)}</pre>
+        ) : (
+          <h3>Please fill out the search form first!</h3>
+        )}
       </>
     )
   }
