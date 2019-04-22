fd62cabdcf02427fbb8499a49b94a8e0580c28d3
Kristen Kriens <kristen.kriens@gmail.com>
2019-04-22 16:23:12 -0400

    Make root reducer file

--- src/index.js
+++ src/index.js
@@ -1,36 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import { connectRouter,routerMiddleware, ConnectedRouter } from 'connected-react-router';
-import { createBrowserHistory } from 'history';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { routerMiddleware, ConnectedRouter } from 'connected-react-router';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
 import './shared/styles/styles.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import generalReducer from './store/reducers/general';
-import authReducer from './store/reducers/auth';
-import navigationReducer from './store/reducers/navigation';
-import modalReducer from './store/reducers/modal';
-import userReducer from './store/reducers/user';
-import geolocateReducer from './store/reducers/geolocate';
-import searchReducer from './store/reducers/search';
+import { rootReducer, history } from './store/reducers/index';
 
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
-
-const history = createBrowserHistory({ basename: '/job-search-companion-react' });
-
-const rootReducer = combineReducers({
-  router: connectRouter(history),
-  general: generalReducer,
-  auth: authReducer,
-  navigation: navigationReducer,
-  modal: modalReducer,
-  user: userReducer,
-  geolocate: geolocateReducer,
-  search: searchReducer
-})
 
 const store = createStore(rootReducer, composeEnhancers(
   applyMiddleware(
---
+++ src/store/reducers/index.js
@@ -0,0 +1,25 @@
+import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router';
+import { createBrowserHistory } from 'history';
+
+import generalReducer from './general';
+import authReducer from './auth';
+import navigationReducer from './navigation';
+import modalReducer from './modal';
+import userReducer from './user';
+import geolocateReducer from './geolocate';
+import searchReducer from './search';
+
+
+export const history = createBrowserHistory({ basename: '/job-search-companion-react' });
+
+export const rootReducer = combineReducers({
+  router: connectRouter(history),
+  general: generalReducer,
+  auth: authReducer,
+  navigation: navigationReducer,
+  modal: modalReducer,
+  user: userReducer,
+  geolocate: geolocateReducer,
+  search: searchReducer
+})
