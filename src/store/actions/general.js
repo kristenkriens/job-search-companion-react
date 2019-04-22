ceb0bdc3a4f5bdf7fea07bb4adf4ea95a37e124b
Kristen Kriens <kristen.kriens@gmail.com>
2019-04-21 20:11:15 -0400

    Remove console.log from previous commit that isn’t needed

--- src/store/actions/general.js
+++ src/store/actions/general.js
@@ -1,7 +1,6 @@
 import * as actionTypes from './actionTypes';
 
 export const updateReduxHandledFormElement = (formElementName, value) => {
-  console.log(value);
   return {
     type: actionTypes.UPDATE_REDUX_HANDLED_FORM_ELEMENT,
     formElementName: formElementName,
