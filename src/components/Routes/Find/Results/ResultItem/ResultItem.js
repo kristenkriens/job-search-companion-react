16553f3961a9c4c228388eefb59ededdd3f780a6
Kristen Kriens <kristen.kriens@gmail.com>
2019-04-26 15:24:22 -0400

    Add expired handling and logged in “track application” link

--- src/components/Routes/Find/Results/ResultItem/ResultItem.js
+++ src/components/Routes/Find/Results/ResultItem/ResultItem.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 import './ResultItem.scss';
 
+import LinkButton from '../../../../UI/Button/LinkButton/LinkButton';
+
 const ResultItem = (props) => {
-  const { result } = props;
-  const { jobtitle, company, formattedLocationFull, snippet, url, formattedRelativeTime } = result;
+  const { result, isAuthenticated } = props;
+  const { jobtitle, company, formattedLocationFull, snippet, url, formattedRelativeTime, expired } = result;
 
   return (
     <div className="result__item">
@@ -18,11 +20,22 @@ const ResultItem = (props) => {
         </div>
         <div className="result__item-top-right">
           <div>{formattedRelativeTime}</div>
+          {expired && (
+            <div>(Expired)</div>
+          )}
         </div>
       </div>
       <div className="result__item-bottom">
         <p dangerouslySetInnerHTML={{__html: snippet}}></p>
-        <a href={url} className="button" target="_blank" rel="noopener noreferrer">More</a>
+        <div class="button-wrapper">
+          <a href={url} className="button" target="_blank" rel="noopener noreferrer">More / Apply</a>
+          {isAuthenticated && (
+            // TODO: Add click handler
+            <LinkButton click="">
+              Track Application
+            </LinkButton>
+          )}
+        </div>
       </div>
     </div>
   )
