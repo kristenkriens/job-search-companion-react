ed4ddb58a143dc2c4e6e0b3de44e3421326fd369
Kristen Kriens <kristen.kriens@gmail.com>
2019-05-04 22:31:01 -0400

    Update url for firebase

--- src/shared/firebaseAxios.js
+++ src/shared/firebaseAxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const firebaseAxios = axios.create({
-  baseURL: 'https://job-search-compa-1514144240150.firebaseapp.com/'
+  baseURL: 'https://job-search-compa-1514144240150.firebaseio.com/'
 });
 
 export default firebaseAxios;
