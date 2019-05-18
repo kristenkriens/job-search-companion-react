ecf68dd1c2c609a36a02571c70a058a9b41f4046
Kristen Kriens <kristen.kriens@gmail.com>
2019-05-17 23:11:01 -0400

    Add and set up chart with data

--- src/components/Routes/Track/Results/Results.js
+++ src/components/Routes/Track/Results/Results.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import isEqual from 'lodash/isEqual';
 
 import LoginRequired from '../../../UI/LoginRequired/LoginRequired';
 import ResultsChart from './ResultsChart/ResultsChart';
 
+import * as actions from '../../../../store/actions/index';
+
 class Results extends Component {
+  componentDidMount = () => {
+    if(this.props.isAuthenticated) {
+      this.props.getSavedApplications(this.props.token, this.props.userId);
+    }
+  }
+
+  count = 0;
+  componentDidUpdate = (prevProps) => {
+    let equal = false;
+    if(this.count > 0) {
+      equal = isEqual(prevProps.savedApplications, this.props.savedApplications);
+    }
+
+    if(this.props.isAuthenticated && !equal) {
+      this.props.getSavedApplications(this.props.token, this.props.userId);
+    }
+
+    this.count++;
+  }
+
   render() {
     const { isAuthenticated, loading, savedApplications } = this.props;
 
@@ -12,18 +35,26 @@ class Results extends Component {
       <>
         {isAuthenticated ? (
           <>
-            <h1>Results</h1>
             {loading ? (
-              <div className="absolute-center">
-                <div className="h3">Your application results are loading!</div>
-                <i className="fa fa-spinner fa-pulse fa-fw fa-2x"></i>
-              </div>
+              <>
+                <h1 className="accessible">Results</h1>
+                <div className="absolute-center">
+                  <div className="h3">Your application results are loading!</div>
+                  <i className="fa fa-spinner fa-pulse fa-fw fa-2x"></i>
+                </div>
+              </>
             ) : (
-              <ResultsChart savedApplications={savedApplications} />
+              <>
+                <h1>Results</h1>
+                <ResultsChart savedApplications={savedApplications} />
+              </>
             )}
           </>
         ) : (
-          <LoginRequired />
+          <>
+            <h1 className="accessible">Results</h1>
+            <LoginRequired />
+          </>
         )}
       </>
     )
@@ -32,9 +63,17 @@ class Results extends Component {
 
 const mapStateToProps = (state) => {
   return {
+    token: state.auth.token,
+    userId: state.auth.userId,
     savedApplications: state.savedApplications.savedApplications,
     loading: state.savedApplications.loading
   }
 }
 
-export default connect(mapStateToProps)(Results);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getSavedApplications: (token, userId) => dispatch(actions.getSavedApplications(token, userId))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Results);
--- src/components/Routes/Track/Results/ResultsChart/ResultsChart.js
+++ src/components/Routes/Track/Results/ResultsChart/ResultsChart.js
@@ -1,13 +1,142 @@
 import React from 'react';
+import { ResponsivePie } from '@nivo/pie';
 
 const ResultsChart = (props) => {
   const { savedApplications } = props;
 
-  console.log(savedApplications);
+  const data = [];
+  savedApplications.forEach((savedApplication) => {
+    data.push(savedApplication.result);
+  });
+
+  const count = data.reduce((total, current) => {
+    total[current] = (total[current] || 0) + 1;
+
+    return total;
+  }, {});
 
   return (
-    <>
-    </>
+    <div style={{height: 400}}>
+      <ResponsivePie
+        data={[
+          {
+            "id": "Interview",
+            "label": "Interview",
+            "value": count['interview']
+          },
+          {
+            "id": "Accepted Offer",
+            "label": "Accepted Offer",
+            "value": count['accepted-offer']
+          },
+          {
+            "id": "Declined Offer",
+            "label": "Declined Offer",
+            "value": count['declined-offer']
+          },
+          {
+            "id": "No Response",
+            "label": "No Response",
+            "value": count['no-response']
+          },
+          {
+            "id": "No Offer",
+            "label": "No Offer",
+            "value": count['no-offer']
+          },
+          {
+            "id": "Declined",
+            "label": "Declined",
+            "value": count['declined']
+          },
+          {
+            "id": "Other",
+            "label": "Other",
+            "value": count['other']
+          },
+          {
+            "id": "None",
+            "label": "None",
+            "value": count['']
+          }
+        ]}
+        colors={['#F05540', '#4ABAA5', '#FFD15C', '#77B5F7']}
+        margin={{ top: 10, right: 100, bottom: 30, left: 100 }}
+        innerRadius={0.5}
+        padAngle={2.5}
+        borderWidth={1}
+        borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.2 ] ] }}
+        radialLabelsSkipAngle={10}
+        radialLabelsTextXOffset={6}
+        radialLabelsTextColor="#344A5E"
+        slicesLabelsSkipAngle={10}
+        slicesLabelsTextColor="#FFFFFF"
+        animate={true}
+        motionStiffness={90}
+        motionDamping={15}
+        defs={[
+          {
+            id: 'lines',
+            type: 'patternLines',
+            background: 'inherit',
+            color: 'rgba(255, 255, 255, 0.1)',
+            rotation: -45,
+            lineWidth: 5,
+            spacing: 10
+          }
+        ]}
+        fill={[
+          {
+            match: {
+                id: 'Interview'
+            },
+            id: 'lines'
+          },
+          {
+            match: {
+                id: 'Accepted Offer'
+            },
+            id: 'lines'
+          },
+          {
+            match: {
+                id: 'Declined Offer'
+            },
+            id: 'lines'
+          },
+          {
+            match: {
+                id: 'No Response'
+            },
+            id: 'lines'
+          },
+          {
+            match: {
+                id: 'No Offer'
+            },
+            id: 'lines'
+          },
+          {
+            match: {
+                id: 'Declined'
+            },
+            id: 'lines'
+          },
+          {
+            match: {
+                id: 'Other'
+            },
+            id: 'lines'
+          },
+          {
+            match: {
+                id: 'None'
+            },
+            id: 'lines'
+          },
+        ]}
+      />
+    </div>
   )
 }
 
